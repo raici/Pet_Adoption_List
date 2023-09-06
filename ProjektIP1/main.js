83221ba9200c5de3a8fbe963d1ce84e26a77e5af
Ivan Raic <raicivan06@gmail.com>
2023-09-06 18:41:47 +0200

    Dodavanje JS

---
+++ ProjektIP1/main.js
@@ -0,0 +1,18 @@
+async function funkcija (){
+    const petPromise = await fetch("https://learnwebcode.github.io/pet-adoption-data/pets.json");
+    const pets = await petPromise.json();
+    
+    const template = document.querySelector("#animal-card")
+
+    const wrapper = document.createElement("div")
+
+    pets.forEach(pet => {
+        const clone = template.content.cloneNode(true)
+
+        wrapper.appendChild(clone)
+    })
+
+    document.querySelector(".animals").appendChild(wrapper)
+}
+
+funkcija(); // Poziv funkcije kako bi se izvršila
