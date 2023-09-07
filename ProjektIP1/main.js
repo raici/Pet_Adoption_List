0ec33365300850de82b8e746490cde9b853086f7
Ivan Raic <raicivan06@gmail.com>
2023-09-07 10:33:15 +0200

    javascript

--- ProjektIP1/main.js
+++ ProjektIP1/main.js
@@ -6,9 +6,34 @@ async function funkcija (){
 
     const wrapper = document.createElement("div")
 
+    function decideAgeText(age) {
+
+        if (!age) {
+            return "Less than a year old "
+        }
+
+        return age > 1 ? `${age} years old ` : "1 year old "
+    }
+
     pets.forEach(pet => {
         const clone = template.content.cloneNode(true)
+        clone.querySelector("h3").textContent = pet.name 
 
+        const img = clone.querySelector("img")
+        img.src= pet.photo
+
+        img.alt = `A ${pet.species} named ${pet.name}`
+
+        const age = new Date().getFullYear() - pet.birthYear
+        const ageText = decideAgeText(age)
+        
+        clone.querySelector(".age").textContent = ageText
+
+        clone.querySelector(".species").textContent = pet.species
+
+        clone.querySelector(".description").textContent = pet.description
+
+        clone.querySelector(".name").textContent = pet.name 
         wrapper.appendChild(clone)
     })
 
